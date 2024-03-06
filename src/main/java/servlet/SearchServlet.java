543139c1a3260aa1cf15f3afc438251ff47136c4
F-akihiko <akichan.0225@icloud.com>
2024-03-06 13:42:47 +0900

    [SearchSelvret］ ﾌｫﾜｰﾄﾞ先のurl修正→ﾌｫﾜｰﾄﾞ成功

--- src/main/java/servlet/SearchServlet.java
+++ src/main/java/servlet/SearchServlet.java
@@ -2,11 +2,14 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.SearchBean;
 
 
 @WebServlet("/search")
@@ -24,6 +27,20 @@ public class SearchServlet extends HttpServlet {
 		
 		String p_name = request.getParameter("p_name");
 		String[] p_categorys = request.getParameterValues("p_category");
+		
+		SearchBean sb;
+		RequestDispatcher rd;
+		if(!(p_name.equals(null))) { //p_nameに値が入っていたらtrue
+			sb = new SearchBean(p_name);
+			request.setAttribute("sb", sb);
+			rd = request.getRequestDispatcher("/WEB-INF/jsp/productSearchResult.jsp");
+		} else {
+			sb = new SearchBean(p_categorys);
+			request.setAttribute("sb", sb);
+			rd = request.getRequestDispatcher("/WEB-INF/jsp/productCategoryResult.jsp");
+		}
+		
+		rd.forward(request, response);
 		
 		
 	}
