c1786219ed6217ba72e9e022b235a87e84e00672
F-akihiko <akichan.0225@icloud.com>
2024-03-06 12:13:08 +0900

    bean 途中

---
+++ src/main/java/model/SearchBean.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.io.Serializable;
+
+public class SearchBean implements Serializable{
+	
+	private String p_name;
+	private String[] p_categorys;
+	
+	public SearchBean() {
+		
+	}
+	public SearchBean(String p_name) {
+		
+	}
+	
+	
+	
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public String[] getP_categorys() {
+		return p_categorys;
+	}
+	public void setP_categorys(String[] p_categorys) {
+		this.p_categorys = p_categorys;
+	}
+	
+	
+}
