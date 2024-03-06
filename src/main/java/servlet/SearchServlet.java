c9b221c5b0eadee65e04d00d8d8554adedab15d6
F-akihiko <akichan.0225@icloud.com>
2024-03-06 16:11:23 +0900

    3/6/16:10 SearchSeavlet NullPointerException解消のため、配列からリストに変換時にif追加

--- src/main/java/servlet/SearchServlet.java
+++ src/main/java/servlet/SearchServlet.java
@@ -1,8 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -29,18 +31,27 @@ public class SearchServlet extends HttpServlet {
 		
 		String p_name = request.getParameter("p_name");
 		String[] p_categorysArray = request.getParameterValues("p_category");
-		List<String> p_categorys = Arrays.asList(p_categorysArray);
+		
+		List<String> p_categorys = new ArrayList<>();
+		if(p_categorysArray == null) {
+			p_categorys.add("0");
+		}else {
+			p_categorys = Arrays.asList(p_categorysArray);
+		}
 		
 		SearchBean sb;
-		RequestDispatcher rd;
-		if(!(p_name == null)) { //p_nameに値が入っていたらtrue
+		RequestDispatcher rd = null;
+		//Object.isNull()→引数がNULLの場合True
+		if(!(Objects.isNull(p_name))) { //p_nameがnullじゃない時true
 			sb = new SearchBean(p_name);
 			request.setAttribute("sb", sb);
 			rd = request.getRequestDispatcher("/WEB-INF/jsp/productSearchResult.jsp");
-		} else {
+		} else if(!(Objects.isNull(p_categorys))) {//p_categorysがnullじゃない時true
 			sb = new SearchBean(p_categorys);
 			request.setAttribute("sb", sb);
 			rd = request.getRequestDispatcher("/WEB-INF/jsp/productCategoryResult.jsp");
+		} else {
+			response.sendRedirect("/EC_Project/searchView.jsp");
 		}
 		
 		rd.forward(request, response);
