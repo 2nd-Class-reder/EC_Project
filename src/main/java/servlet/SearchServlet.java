b929128abac29c5fc4e577c3e5659ef10a67dcc5
F-akihiko <akichan.0225@icloud.com>
2024-03-06 16:16:11 +0900

    3/6/16:14 SearchServlet ﾌｫﾜｰﾄﾞ分岐時のnullチェックのリファクタリング

--- src/main/java/servlet/SearchServlet.java
+++ src/main/java/servlet/SearchServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -42,11 +41,11 @@ public class SearchServlet extends HttpServlet {
 		SearchBean sb;
 		RequestDispatcher rd = null;
 		//Object.isNull()→引数がNULLの場合True
-		if(!(Objects.isNull(p_name))) { //p_nameがnullじゃない時true
+		if(p_name != null) { //p_nameがnullじゃない時true
 			sb = new SearchBean(p_name);
 			request.setAttribute("sb", sb);
 			rd = request.getRequestDispatcher("/WEB-INF/jsp/productSearchResult.jsp");
-		} else if(!(Objects.isNull(p_categorys))) {//p_categorysがnullじゃない時true
+		} else if(p_categorys != null) {//p_categorysがnullじゃない時true
 			sb = new SearchBean(p_categorys);
 			request.setAttribute("sb", sb);
 			rd = request.getRequestDispatcher("/WEB-INF/jsp/productCategoryResult.jsp");
