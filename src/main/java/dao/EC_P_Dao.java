a11dd59ede2cc0924fe48792741410ffbaf8b589
F-akihiko <akichan.0225@icloud.com>
2024-03-14 17:16:17 +0900

    バックアップ

---
+++ src/main/java/dao/EC_P_Dao.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class EC_P_Dao {
+	private final String JDBC_URL =
+			"dpg-cn8opo0cmk4c739s8ig0-a.singapore-postgres.render.com";
+	private final String DB_USER = "render";
+	private final String DB_PASS = "GwHkVIfxFzKuRfT26uTbVLphSw6IpmcU";
+	
+	
+}
